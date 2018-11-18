10b8362504523c1ae04e4e38f2c685eb3b166bf0
Vlad104 <vlad104da@gmail.com>
2018-11-19 02:20:46 +0300

    работа с битами. начало

--- Coder.cpp
+++ Coder.cpp
@@ -1,4 +1,5 @@
 #include "Coder.h"
+#include <vector>
 
 void Coder::set_input_data_size(const int size) {
 
@@ -7,4 +8,20 @@ void Coder::set_input_data_size(const int size) {
 
 int Coder::get_output_size() {
 	
+}
+
+void Coder::write_bit(std::vector<char>& data, bool bit, int& bits_count) {
+	if (bits_count == 0) {
+		data.push_back(0);
+	}
+	if (bit) {
+		data.back() |= 1 << bits_count;
+	}
+	bits_count = (bits_count + 1) % 8;
+}
+
+void Coder::write_byte(char value) {
+	for (int i = 0; i < 8; i++) {
+		write_bit( (value & (1 << i)) != 0 );
+	}
 }
--- LZW.cpp
+++ LZW.cpp
@@ -1,4 +1,6 @@
 #include "LZW.h"
+#include <map>
+#include <cmath>
 
 void LZW::encode(const std::string& data, std::back_insert_iterator result, dictionary_size) {
 
@@ -64,11 +66,25 @@ std::string LZW::decode(std::back_insert_iterator begin, std::back_insert_iterat
 char* LZW::compress(const char* data, int size) {
 	std::string str_data(data, size);
 	std::vector<int> encoded;
+	std::vector<char> result;
 	int dict_size = 256;
 	encode(str_data, encoded, dict_size);
 
-	// translate vector<int> to bits !!!
-	//return bits
+	int fill = 0; // количество записанных бит в байте
+	for (int i = 0; i < size; i++) {
+		int value = encoded[i];
+		int bits_count = log(value) / log(2); // количество значащих бит в числе
+		for (int j = 0; j < bits_count; j++) { // не уверен, что учел выход за 8 бит !!!!!!
+			if (fill == 0) {
+				result.push_back(0);
+			}
+			result.back() |= ( (value >> j) & 1u ) << (7 - fill); // "конкатенируем" значащие биты
+			fill = (fill + 1) % 8;
+		}
+	}
+
+	//return result;
+
 }
 
 
