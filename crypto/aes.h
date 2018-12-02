51a2c95f47f945b7ad8ccdcc307ce286b9de4385
DmitriyPrischep <dmitriygoogle@yandex.ru>
2018-12-02 23:53:07 +0300

    Whole implementation of the AES

--- crypto/aes.h
+++ crypto/aes.h
@@ -6,40 +6,64 @@
 typedef unsigned char byte;
 typedef std::vector<std::vector<byte>> ByteArray;
 
-class AES final
+
+typedef std::vector<std::vector<unsigned char>> ByteArray;
+typedef unsigned char byte;
+
+class ICrypto
 {
 public:
-    AES();
-    AES(const int sizeKey);
+    ICrypto() {}
+    virtual ~ICrypto(){}
+    virtual const std::vector<byte> encrypt(const std::vector<byte>& buffer) = 0;
+    virtual const std::vector<byte> decrypt(const std::vector<byte>& buffer) = 0;
+    virtual const std::vector<byte> generate_key(const std::string password) = 0;
+};
+
+//AES 128 default
+class AES final : ICrypto{
+public:
+//    AESEncryptor(size_t key_length) : key(GenerateKey(key_length)) {}
+    AES(const size_t size_key, const std::string& pass, const short nk, const short nr);
+    AES(const AES& algorithm) = delete;
+    AES(const AES&& algorithm) = delete;
+    AES& operator =(const AES& algorithm) = delete;
+    AES& operator =(const AES&& algorithm) = delete;
     ~AES();
-    unsigned char xor_byte(byte first, byte second);
-    void xor_byte_block(ByteArray& block, ByteArray key);
-    byte sub_byte(byte value, bool direction);
+
+    const std::vector<byte> encrypt(const std::vector<byte>& buffer);
+    const std::vector<byte> decrypt(const std::vector<byte>& buffer);
+    const std::vector<byte> generate_key(const size_t size_key, const std::string password);
+
+    static unsigned char xor_byte(byte first, byte second);
+    static byte get_s_element(size_t x, size_t y, bool direction);
+    static byte get_c_element(size_t x, size_t y, bool direction);
+    static byte get_r_con(size_t x, size_t y);
+
+private:
+    void encrypt_block(ByteArray& block);
+    void decrypt_block(ByteArray& block);
+
     void sub_bytes(ByteArray& block, bool direction);
     void shift_rows(ByteArray& block, bool direction);
     void mix_columns(ByteArray& block, bool direction);
     byte mul_galois(byte a, byte b);
 
-    static byte get_s_element(size_t x, size_t y, bool direction);
-    static byte get_c_element(size_t x, size_t y, bool direction);
-    static byte get_r_gon(size_t x, size_t y);
+    void xor_byte_block(ByteArray& block, ByteArray key);
+    byte sub_byte(byte value, bool direction);
 
-private:
-    void init_s_box();
-    void init_inv_s_box();
-    void init_r_con();
-    void init_c_box();
-    void init_inv_c_box();
-    Key key;
+    Key* key;
 
-    static const int count_round = 10;
-    static const int length_word = 4;
-    static const int count_word = 4;
     static const ByteArray s_box;
     static const ByteArray inv_s_box;
     static const ByteArray c_box;
     static const ByteArray inv_c_box;
     static const ByteArray r_con;
+
+    //128-bit settings
+    const int count_round = 10;
+    const int length_word = 4;
+    const int count_word = 4;
 };
 
 #endif // AES_H
--- crypto/key.h
+++ crypto/key.h
@@ -6,19 +6,22 @@
 typedef unsigned char byte;
 typedef std::vector<std::vector<byte>> ByteArray;
 
-//5 rules
-
 class Key final
 {
 public:
-    Key();
-    Key(const std::string pass, const size_t key_size, size_t nb, size_t nk, size_t nr);
-    void set_key(std::string pass_string, size_t key_size);
-    ByteArray get_matrix_key(size_t index);
+    Key(const std::string& pass, const size_t key_size, size_t nb, size_t nk, size_t nr);
+    Key(const Key& key) = delete;
+    Key(const Key&& key) = delete;
+    Key& operator =(const Key& key) = delete;
+    Key& operator =(const Key&& key) = delete;
+    ~Key() = default;
+
+    void set_key(const std::string& pass_string, size_t key_size);
 
 private:
-    std::vector<byte> shift_word(const std::vector<byte>& word);
-    std::vector<byte> sub_word(const std::vector<byte>& word);
+    ByteArray get_matrix_key(size_t index);
+    std::vector<byte> shift_word(const std::vector<byte> word);
+    std::vector<byte> sub_word(const std::vector<byte> word);
     ByteArray get_encrypt_key(std::vector<byte> pass);
 
     ByteArray data_key;
