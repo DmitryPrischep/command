448cbf1ea9d818477df9c1c26455b2c1bf787237
Vlad104 <vlad104da@gmail.com>
2018-12-17 21:48:29 +0300

    комит для ревью

--- compressor/AdaptiveHuffman.hpp
+++ compressor/AdaptiveHuffman.hpp
@@ -33,4 +33,29 @@ private:
 	void update_tree(Node* node) noexcept;
 	void exchange(Node* a, Node* b) noexcept;
 	Node* highest_node(Node* node) const noexcept;
+
+protected:
+	// структура узла для дерева Хаффмана
+	struct Node {
+		char byte;
+		int number;
+		int weight;
+
+		Node* parent;
+		Node* left;
+		Node* right;
+
+		Node(char _byte, int _number, int _weight = 0, Node* _parent = nullptr, Node* _left = nullptr, Node* _right = nullptr) :
+				byte(_byte), 
+				number(_number),
+				weight(_weight), 
+				parent(_parent),
+				left(_left),
+				right(_right)
+		{}
+		~Node() {
+			delete left;
+			delete right;
+		}
+	};
 };
